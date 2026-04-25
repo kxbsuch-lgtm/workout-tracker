7695870a7374ac1b644e47577763631f70aa0b90
kxbsuch-lgtm <kxbsuch@gmail.com>
2026-04-25 17:44:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const Storage = {
+get(key, def){
+try{
+return JSON.parse(localStorage.getItem(key)) ?? def;
+}catch{
+return def;
+}
+},
+set(key,val){
+localStorage.setItem(key,JSON.stringify(val));
+}
+};
+
 (function(){
 
 function today(){
