33a1d05fc89c524424238a9e7c0d12ccae7f69db
kxbsuch-lgtm <kxbsuch@gmail.com>
2026-04-25 17:47:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const Storage = {
-get(key, def){
-try{
-return JSON.parse(localStorage.getItem(key)) ?? def;
-}catch{
-return def;
-}
-},
-set(key,val){
-localStorage.setItem(key,JSON.stringify(val));
-}
-};
-
+alert("app.js start");
+console.log("Storage =", typeof Storage);
 (function(){
 
 function today(){
